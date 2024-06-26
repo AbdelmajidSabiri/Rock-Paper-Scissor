e99c304a8f179123bdfcea30b6b71062612f5e4f
Abdelmajid Sabiri <abdlmajidsabiri26@gmail.com>
2024-06-26 20:43:29 +0100

    add some js code to take the user chois

--- script.js
+++ script.js
@@ -4,5 +4,16 @@ cpuResult = document.querySelector(".cpu_result img")
 result = document.querySelector(".result")
 optionImages = document.querySelectorAll(".option_image")
 
-console.log(gameContainer, userResult,cpuResult,result,optionImages)
 
+optionImages.forEach((image,index) =>{
+    image.addEventListener("click", (e) => {
+        image.classList.add("active");
+
+        optionImages.forEach((image2, index2) => {
+            index !== index2 && image2.classList.remove("active");
+        });
+
+        let imageSrc = e.target.querySelector("img").src;
+        userResult.src = imageSrc
+    });
+});
