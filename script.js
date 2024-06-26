330fc54fec8ddf226befd6ccfd72563fb10e3b8a
Abdelmajid Sabiri <abdlmajidsabiri26@gmail.com>
2024-06-26 20:49:38 +0100

    add comments to explain

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ cpuResult = document.querySelector(".cpu_result img")
 result = document.querySelector(".result")
 optionImages = document.querySelectorAll(".option_image")
 
+//loop through each option image element
 
 optionImages.forEach((image,index) =>{
     image.addEventListener("click", (e) => {
+        //Add class "active" to pressed image
         image.classList.add("active");
 
         optionImages.forEach((image2, index2) => {
+            // if the current index doesn't match the clicked index
+            // Remove the "active" class from the other option images
             index !== index2 && image2.classList.remove("active");
         });
 
         let imageSrc = e.target.querySelector("img").src;
         userResult.src = imageSrc
+
+        //generate random number between 0 and 2
+        let randomNumber = Math.floor(Math.random() * 3)
     });
 });
